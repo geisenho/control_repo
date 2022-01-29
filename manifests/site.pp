e3bb2bb5bedc18281bfe84765d952ddcb97999bb
geisenho <72321875+geisenho@users.noreply.github.com>
2022-01-29 17:04:07 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -4,7 +4,7 @@ node 'puppet1.eisenhour.net' {
   include role::master_server
   file { '/root/README':
     ensure => file,
-    content => $fqdn,
+    content => "Welcome to ${fqdn}",
   }
 }
 node /^web/ {
