c0a208e6b9ad65d22ce8552ef53e984a67e324a6
geisenho <72321875+geisenho@users.noreply.github.com>
2022-03-06 22:12:36 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -10,12 +10,16 @@ class minecraft {
      ensure => present,
    }
    file {'/opt/minecraft/eula.txt':
-   ensure => file,
-   content => 'eula=true',
+     ensure => file,
+     content => 'eula=true',
    }
    file {'/etc/systemd/system/minecraft.service':
-   ensure => file,
-   source => 'puppet:///modules/minecraft/minecraft.service',
+     ensure => file,
+     source => 'puppet:///modules/minecraft/minecraft.service',
+   }
+   service{minecraft':
+     ensure => running,
+     enable => true,
    }
 }
  
